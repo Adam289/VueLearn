b47b6f412110b1e08ada3b3ab7a788bf689c21cf
Van-Ming <646102528@qq.com>
2020-01-03 23:59:22 +0800

    html改为单文件组件

---
+++ src/App1.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+    <ol>
+        <todo-item v-for="item in groceryList" v-bind:todo="item" v-bind:key="item.id"></todo-item>
+    </ol>
+    </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+export default {
+    name:'app',
+    data(){
+        return{
+            groceryList: [
+                {
+                    id:0,
+                    text:'蔬菜'
+                },{
+                    id:1,
+                    text:'蛋糕'
+                },{
+                    id:2,
+                    text:'牛奶'
+                }
+            ]
+        }
+    },
+    components:{
+        TodoItem
+    }
+}
+</script>
